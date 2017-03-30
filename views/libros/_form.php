f271950cd687a42aa77a54c6dace7ba73b82888f
Alejandro Lopez <lopalejandro@gmail.com>
2017-03-30 12:42:19 -0300

    Arreglado un error en la creacion de libros

--- views/libros/_form.php
+++ views/libros/_form.php
@@ -19,28 +19,28 @@ use kartik\widgets\Select2;
 
     <?php $form = ActiveForm::begin(['type' => ActiveForm::TYPE_HORIZONTAL]); ?>
     <div class="form-group">
-    <?= $form->field($model, 'titulo',['labelOptions'=>['class'=>'col-sm-2 col-md-2']]); ?> 
-    <?= $form->field($model, 'nro_libro',['labelOptions'=>['class'=>'col-sm-2 col-md-2']])->textInput(); ?>
-    <?= $form->field($model, 'ano',['labelOptions'=>['class'=>'col-sm-2 col-md-2']])->textInput(); ?>
-    <?= $form->field($model, 'tipo_libro_id', ['labelOptions'=>['class'=>'col-sm-2 col-md-2']])->widget(Select2::classname(), [
+        <?= $form->field($model, 'titulo',['labelOptions'=>['class'=>'col-sm-2 col-md-2']]); ?> 
+        <?= $form->field($model, 'nro_libro',['labelOptions'=>['class'=>'col-sm-2 col-md-2']])->textInput(); ?>
+        <?= $form->field($model, 'ano',['labelOptions'=>['class'=>'col-sm-2 col-md-2']])->textInput(); ?>
+        <?= $form->field($model, 'tipo_libro_id', ['labelOptions'=>['class'=>'col-sm-2 col-md-2']])->widget(Select2::classname(), [
             'data'=>ArrayHelper::map(TipoLibro::find()->all(),'tipo_tipo_libro_id','descripcion'),
             'pluginOptions'=>['allowClear'=>true],
             'options' => ['placeholder'=>'Seleccione Uno']
-        ]); ?>
-    <?= $form->field($model, 'edicion',['labelOptions'=>['class'=>'col-sm-2 col-md-2']])->textInput(); ?>
-    <?= $form->field($model, 'editorial',['labelOptions'=>['class'=>'col-sm-2 col-md-2']])->textInput(); ?>
-	</div>
-	<div class="form-group">
-    <div class="col-sm-offset-2 col-sm-10">
-        <hr>
-<?php        
-        echo Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'),
-        ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']);
-?>    
+            ]); ?>
+        <?= $form->field($model, 'edicion',['labelOptions'=>['class'=>'col-sm-2 col-md-2']])->textInput(); ?>
+        <?= $form->field($model, 'editorial',['labelOptions'=>['class'=>'col-sm-2 col-md-2']])->textInput(); ?>
     </div>
-</div>
-<?php
-    ActiveForm::end(); 
+    <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+            <hr>
+            <?php        
+                echo Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'),
+                ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']);
+            ?>    
+        </div>
+    </div>
+    <?php
+        ActiveForm::end(); 
     ?>
 
 </div>
