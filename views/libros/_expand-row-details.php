c32a327440f1e102f4fa6fc4ada8ddc1211013a9
Alejandro Lopez <lopalejandro@gmail.com>
2017-04-20 13:16:35 -0300

    cambio 2

--- views/libros/_expand-row-details.php
+++ views/libros/_expand-row-details.php
@@ -4,19 +4,43 @@ use yii\helpers\Html;
 use kartik\widgets\ActiveForm;
 use kartik\builder\Form;
 use kartik\datecontrol\DateControl;
+use kartik\grid\GridView;
 use kartik\select2\Select2;
 use yii\helpers\ArrayHelper;
 use app\models\Autor;
 use app\models\TipoLibro;
 use app\models\Copias;
 
+
+$this->title = 'Allegatos';
+$this->params['breadcrumbs'][] = $this->title;
 ?>
 
-<div class="copias-view">
+    <div class="allegato-index">
 
-<?php echo Html::encode(print_r($model->copias));?>
+        <h1><?= Html::encode($this->title) ?></h1>
+        <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
+        <p>
+            <?= Html::a('Create Allegato', ['create'], ['class' => 'btn btn-success']) ?>
+        </p>
 
-    
+        <?php
 
-</div>
+         GridView::widget([
+            'dataProvider' => $dataProvider,
+            'filterModel' => $searchModel,
+            'columns' => [
+                ['class' => 'yii\grid\SerialColumn'],
+
+                'copias_id',
+                'estado_id',
+                'nro_copia',
+
+                ['class' => 'yii\grid\ActionColumn'],
+            ],
+        ]);
+        
+         ?>
+
+    </div>
